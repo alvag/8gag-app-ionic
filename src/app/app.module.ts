5801fed0b33096f669584e11a60713f64c9c3704
Max Alva <alva85@gmail.com>
2018-09-04 23:54:40 -0500

    Conexion a Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { UploadPage } from '../pages/upload/upload';
+
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
 
 @NgModule({
     declarations: [
@@ -16,7 +29,10 @@ import { UploadPage } from '../pages/upload/upload';
     ],
     imports: [
         BrowserModule,
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule
     ],
     bootstrap: [IonicApp],
     entryComponents: [
@@ -27,6 +43,7 @@ import { UploadPage } from '../pages/upload/upload';
     providers: [
         StatusBar,
         SplashScreen,
+        AngularFireDatabase,
         { provide: ErrorHandler, useClass: IonicErrorHandler }
     ]
 })
